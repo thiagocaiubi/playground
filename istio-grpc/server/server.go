fd0e0b74e0b7cfe615bfca429ca695db892c3308
Thiago Caiubi <thiago.caiubi@gmail.com>
2022-05-19 16:07:42 -0300

    istio-grpc: istio load balacing grpc requests

---
+++ istio-grpc/server/server.go
@@ -0,0 +1,93 @@
+/*
+ *
+ * Original work Copyright 2015 gRPC authors
+ * Modified work Copyright 2021 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+/*
+ * Changes:
+ * 2019-06-24: SayHello returns hostname in response header
+ * 2021-04-15: Add UnimplementedGreeterServer as required for forward compatibility
+ */
+
+//go:generate protoc -I ../helloworld --go_out=plugins=grpc:../helloworld ../helloworld/helloworld.proto
+
+// Package main implements a server for Greeter service.
+package main
+
+import (
+	"context"
+	"flag"
+	"log"
+	"net"
+	"os"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	pb "google.golang.org/grpc/examples/helloworld/helloworld"
+	healthpb "google.golang.org/grpc/health/grpc_health_v1"
+	"google.golang.org/grpc/metadata"
+	"google.golang.org/grpc/status"
+)
+
+type greeterServer struct {
+	pb.UnimplementedGreeterServer
+}
+
+// SayHello implements helloworld.GreeterServer
+func (s *greeterServer) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloReply, error) {
+	log.Printf("Received: %v", in.Name)
+	// [START istio_sample_apps_grpc_greeter_go_server_hostname]
+	hostname, err := os.Hostname()
+	if err != nil {
+		log.Printf("Unable to get hostname %v", err)
+	}
+	if hostname != "" {
+		grpc.SendHeader(ctx, metadata.Pairs("hostname", hostname))
+	}
+	// [END istio_sample_apps_grpc_greeter_go_server_hostname]
+	log.Printf("Replying from: %v", hostname)
+	return &pb.HelloReply{Message: "Hello " + in.Name}, nil
+}
+
+type healthServer struct{}
+
+// Check is used for health checks
+func (s *healthServer) Check(ctx context.Context, in *healthpb.HealthCheckRequest) (*healthpb.HealthCheckResponse, error) {
+	log.Printf("Handling Check request [%v]", in, ctx)
+	return &healthpb.HealthCheckResponse{Status: healthpb.HealthCheckResponse_SERVING}, nil
+}
+
+// Watch is not implemented
+func (s *healthServer) Watch(in *healthpb.HealthCheckRequest, srv healthpb.Health_WatchServer) error {
+	return status.Error(codes.Unimplemented, "Watch is not implemented")
+}
+
+func main() {
+	address := flag.String("address", ":50051", "address to listen on")
+	flag.Parse()
+	lis, err := net.Listen("tcp", *address)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	pb.RegisterGreeterServer(s, &greeterServer{})
+	healthpb.RegisterHealthServer(s, &healthServer{})
+	log.Printf("Listening on address %s", *address)
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
